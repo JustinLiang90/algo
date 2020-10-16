3f904bd1a832484d03aecaec14f27a9ff515f323
Justin Liang <justin@sneakql.com>
2020-10-16 20:28:33 +1100

    Leetcode438

--- src/com/justin/twopointers/LeetCode438.java
+++ src/com/justin/twopointers/LeetCode438.java
@@ -1,2 +1,50 @@
-package com.justin.twopointers;public class LeetCode438 {
+package com.justin.twopointers;
+
+import java.util.*;
+
+public class LeetCode438 {
+    public static List<Integer> findAnagrams(String s, String p) {
+        List<Integer> result = new ArrayList<>();
+
+        Map<Character, Integer> targetMap = new HashMap<>();
+        for(char c: p.toCharArray()) {
+            targetMap.put(c, targetMap.getOrDefault(c, 0) + 1);
+        }
+
+        int j = 0;
+        Map<Character, Integer> map = new HashMap<>();
+
+        int count = 0;
+        for (int i = 0; i < s.length(); i++) {
+            char rightChar = s.charAt(i);
+            if (targetMap.containsKey(rightChar)) {
+                map.put(rightChar, map.getOrDefault(rightChar, 0) + 1);
+                if (map.get(rightChar).equals(targetMap.get(rightChar))) {
+                    count++;
+                }
+            }
+
+            while(count == targetMap.size()) {
+                if (i - j + 1 == p.length()) {
+                    result.add(j);
+                }
+
+                char leftChar = s.charAt(j);
+                if (map.containsKey(leftChar)) {
+                    map.put(leftChar, map.get(leftChar) - 1);
+                    if (map.get(leftChar) < targetMap.get(leftChar)) {
+                        count--;
+                    }
+                }
+                j++;
+            }
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        List<Integer> result = findAnagrams("ababababab", "aab");
+        System.out.println(Arrays.stream(result.toArray()));
+    }
 }
