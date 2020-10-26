9c3f66ccc60f4405337f42664a17a6db0e706db0
justin-liang <justin@sneakql.com>
2020-10-26 19:37:42 +1100

    Leetcode209

---
+++ src/com/justin/twopointers/Leetcode209.java
@@ -0,0 +1,30 @@
+package com.justin.twopointers;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Type: presum/sliding window.
+ * https://leetcode.com/problems/minimum-size-subarray-sum/
+ */
+public class Leetcode209 {
+  public static int minSubArrayLen(int s, int[] nums) {
+    int j = 0, result = Integer.MAX_VALUE, sum = 0;
+
+    for (int i = 0; i < nums.length; i++) {
+      sum += nums[i];
+      while(sum >= s) {
+        result = Math.min(result, i - j + 1);
+        sum -= nums[j];
+        j++;
+      }
+    }
+
+    return result == Integer.MAX_VALUE ? 0 : result;
+  }
+
+  public static void main(String[] args) {
+    int result = minSubArrayLen(7, new int[] { 2,3,1,2,4,3 });
+    System.out.println(result);
+  }
+}
